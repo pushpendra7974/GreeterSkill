3a3024d24c278b2c80bdd8d91d7db1387bbf77e1
pushpendra7974 <pushpendra7974@hotmail.com>
2019-03-07 18:03:53 +0530

    Session Feature Integrated

--- lambdaFun/index.js
+++ lambdaFun/index.js
@@ -5,6 +5,11 @@ exports.handler = function(event,context){
     try
     {
         var request = event.request;
+        var session = event.session;
+
+        if(!event.session.attributes){
+            event.session.attributes={};
+        }
 
         /**
          * Types of Requests:-
@@ -14,29 +19,30 @@ exports.handler = function(event,context){
          **/
 
         if(request.type === "LaunchRequest"){
-            let options ={};
-            options.speechText = "Welcome to Greetings skill. Using our skill you can greet your guests. Whom you want to greet?";
-            options.repromptText ="You can say for example, say hello to John.";
-            options.endSession = "false";
 
-            context.succeed(buildResponse(options));
+            handleLaunchRequest(context);
 
         }else if(request.type === "IntentRequest"){
-
-            let options ={};
+            
             if(request.intent.name === "HelloIntent"){
-                let name = request.intent.slots.FirstName.value;
-                options.speechText = "Hello "+ name + ". ";
-                options.speechText += getWish();
-                getQuote(function(quote,error){
-                    if(error){
-                        context.fail(error);
-                    }else{
-                        options.speechText += quote;
-                        options.endSession = true;
-                        context.succeed(buildResponse(options));
-                    }
-                });                
+                
+                handleHelloIntent(request, context);                
+            
+            }else if(request.intent.name === "QuoteIntent"){
+
+                handleQuoteIntent(request, context, session);
+
+            }else if(request.intent.name === "NextQuoteIntent"){
+                
+                handleNextQuoteIntent(request, context, session);
+
+            }else if(request.intent.name === "AMAZON.StopIntent"||request.intent.name === "AMAZON.CancelIntent"){
+            
+                context.succeed(buildResponse({
+                    speechText:"Good Bye. See you again next time. ",
+                    endSession :true
+                }));
+
             }else{
                 throw "Unknown Intent Type";
             }
@@ -50,6 +56,75 @@ exports.handler = function(event,context){
     catch(e)
     {
         context.fail("Exception : "+ e);
+    }
+}
+
+function handleHelloIntent(request, context) {
+    let options = {};
+    let name = request.intent.slots.FirstName.value;
+    options.speechText = "Hello " + name + ". ";
+    options.speechText += getWish();
+    getQuote(function (quote, error) {
+        if (error) {
+            context.fail(error);
+        }
+        else {
+            options.speechText += quote;
+            options.endSession = true;
+            context.succeed(buildResponse(options));
+        }
+    });
+}
+
+function handleLaunchRequest(context) {
+    let options = {};
+    options.speechText = "Welcome to Greetings skill. Using our skill you can greet your guests. Whom you want to greet?";
+    options.repromptText = "You can say for example, say hello to John.";
+    options.endSession = "false";
+    context.succeed(buildResponse(options));
+}
+
+function handleQuoteIntent(request, context, session){
+    let options = {};
+    options.session = session;
+
+    getQuote(function (quote, error) {
+        if (error) {
+            context.fail(error);
+        }
+        else {
+            options.speechText = quote;
+            options.speechText += "Do you want to listen one more quote? ";
+            options.repromptText = "You can say yes or one more. ";
+            options.session.attributes.quoteIntent = true;
+            options.endSession = false;
+            context.succeed(buildResponse(options));
+        }
+    });    
+}
+
+function handleNextQuoteIntent(request, context, session){
+    let options = {};
+    options.session = session;
+
+    if(session.attributes.quoteIntent){
+        getQuote(function (quote, error) {
+            if (error) {
+                context.fail(error);
+            }
+            else {
+                options.speechText = quote;
+                options.speechText += "Do you want to listen one more quote? ";
+                options.repromptText = "You can say yes or one more. ";
+                //options.session.attributes.quoteIntent = true;
+                options.endSession = false;
+                context.succeed(buildResponse(options));                
+            }
+        });  
+    }else{
+        options.speechText = "Sorry! It's a wrong invocation of this intent";
+        options.endSession = true;
+        context.succeed(buildResponse(options));
     }
 }
 
@@ -110,6 +185,10 @@ function buildResponse(options){
                 ssml : "<speak>"+options.repromptText+"</speak>"
             }
         };
+    }
+
+    if(options.session && options.session.attributes){
+        response.sessionAttributes = options.session.attributes;
     }
 
     return response;
